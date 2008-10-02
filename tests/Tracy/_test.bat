b6f5c20623a6c1eef12998dcb1dec6d4f6d66c33
David Grudl <david@grudl.com>
2008-10-02 10:22:58 +0000

    updated test

--- tests/Tracy/_test.bat
+++ tests/Tracy/_test.bat
@@ -1,6 +1,6 @@
 call ../config.bat
 
-for %%f in (test.*.php) do "%php%" -q "%%f" > "output\%%f.html"
+for %%f in (test.*.php) do %php% -q "%%f" > "output\%%f.html"
 
 "%wget%" "%testUri%/Debug/test.firefox.php" -O output\test.firefox.php.html
 
