a0b158d30f5d291c3428dd5cfef77f732bf39ccc
David Grudl <david@grudl.com>
2013-03-19 16:15:41 +0100

    added loader tracy.php

---
+++ src/tracy.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/Tracy/IBarPanel.php';
+require __DIR__ . '/Tracy/Bar.php';
+require __DIR__ . '/Tracy/BlueScreen.php';
+require __DIR__ . '/Tracy/DefaultBarPanel.php';
+require __DIR__ . '/Tracy/Dumper.php';
+require __DIR__ . '/Tracy/ErrorException.php';
+require __DIR__ . '/Tracy/FireLogger.php';
+require __DIR__ . '/Tracy/Helpers.php';
+require __DIR__ . '/Tracy/Logger.php';
+require __DIR__ . '/Tracy/Debugger.php';
+require __DIR__ . '/Tracy/shortcuts.php';
