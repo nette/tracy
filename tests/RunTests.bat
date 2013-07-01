7a28b01635e47fd6a6c5b51b3210c45449eed942
David Grudl <david@grudl.com>
2013-07-01 17:32:55 +0200

    updated test files

--- tests/RunTests.bat
+++ tests/RunTests.bat
@@ -1,15 +1,11 @@
 @ECHO OFF
 
-SET testRunner="%~dp0..\vendor\nette\tester\Tester\tester.php"
-
-IF NOT EXIST %testRunner% (
+IF NOT EXIST "%~dp0..\vendor\nette\tester" (
 	ECHO Nette Tester is missing. You can install it using Composer:
 	ECHO php composer.phar update --dev
 	EXIT /B 2
 )
 
-SET phpIni="%~dp0php.ini-win"
-
-php.exe -c %phpIni% %testRunner% -p php-cgi.exe -c %phpIni% -j 20 -log "%~dp0test.log" %*
+php.exe -n "%~dp0..\vendor\nette\tester\Tester\tester.php" -p php-cgi.exe -c "%~dp0php-win.ini" -j 20 -log "%~dp0test.log" %*
 
 rmdir "%~dp0/tmp" /S /Q
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -11,26 +11,36 @@ if [ ! -f "$runnerScript" ]; then
 	exit 2
 fi
 
-# Path to php.ini if passed as argument option
-phpIni=
-while getopts ":c:" opt; do
-	case $opt in
-	c)	phpIni="$OPTARG"
-		;;
+# Default runner arguments
+jobsNum=20
+phpIni="$dir/php-unix.ini"
 
-	:)	echo "Missing argument for -$OPTARG option" >&2
-		exit 2
-		;;
-	esac
+# Command line arguments processing
+for i in `seq 1 $#`; do
+	if [ "$1" = "-j" ]; then
+		shift
+		if [ -z "$1" ]; then
+			echo "Missing argument for -j option." >&2
+			exit 2
+		fi
+		jobsNum="$1"
+
+	elif [ "$1" = "-c" ]; then
+		shift
+		if [ -z "$1" ]; then
+			echo "Missing argument for -c option." >&2
+			exit 2
+		fi
+		phpIni="$1"
+
+	else
+		set -- "$@" "$1"
+	fi
+	shift
 done
 
-# Runs tests with script's arguments, add default php.ini if not specified
-# Doubled -c option intentionally
-if [ -n "$phpIni" ]; then
-	php -c "$phpIni" "$runnerScript" -j 20 "$@"
-else
-	php -c "$dir/php.ini-unix" "$runnerScript" -j 20 -c "$dir/php.ini-unix" "$@"
-fi
+# Run tests with script's arguments, doubled -c option intentionally
+php -c "$phpIni" "$runnerScript" -j "$jobsNum" -c "$phpIni" "$@"
 error=$?
 
 # Print *.actual content if tests failed
