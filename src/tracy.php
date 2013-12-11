40dd613acb0dc3fa1b733a10590ca27a54f466d2
David Grudl <david@grudl.com>
2013-12-11 15:44:41 +0100

    Released version 0.9.0

--- src/tracy.php
+++ src/tracy.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Tracy (version 0.9.0, http://tracy.nette.org)
+ *
+ * Copyright (c) 2004, 2013 David Grudl (http://davidgrudl.com)
+ */
+
 require __DIR__ . '/Tracy/IBarPanel.php';
 require __DIR__ . '/Tracy/Bar.php';
 require __DIR__ . '/Tracy/BlueScreen.php';
