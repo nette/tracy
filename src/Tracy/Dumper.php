nette/nette

Dumper: fixed dumping INF and NAN

Nette/Diagnostics/Dumper.php
@@ -143,8 +143,10 @@ class Dumper
 
 	private static function dumpDouble(& $var)
 	{
-		$var = json_encode($var);
-		return '<span class="nette-dump-number">' . $var . (strpos($var, '.') === FALSE ? '.0' : '') . "</span>\n";
+		$var = is_finite($var)
+			? ($tmp = json_encode($var)) . (strpos($tmp, '.') === FALSE ? '.0' : '')
+			: var_export($var, TRUE);
+		return "<span class=\"nette-dump-number\">$var</span>\n";
 	}
 
 

tests/Nette/Diagnostics/Dumper.toHtml().phpt
@@ -44,6 +44,15 @@ Assert::match( '<pre class="nette-dump"><span class="nette-dump-number">0.0</spa
 Assert::match( '<pre class="nette-dump"><span class="nette-dump-number">0.1</span>
 </pre>', Dumper::toHtml(0.1) );
 
+Assert::match( '<pre class="nette-dump"><span class="nette-dump-number">INF</span>
+</pre>', Dumper::toHtml(INF) );
+
+Assert::match( '<pre class="nette-dump"><span class="nette-dump-number">-INF</span>
+</pre>', Dumper::toHtml(-INF) );
+
+Assert::match( '<pre class="nette-dump"><span class="nette-dump-number">NAN</span>
+</pre>', Dumper::toHtml(NAN) );
+
 Assert::match( '<pre class="nette-dump"><span class="nette-dump-string">""</span>
 </pre>', Dumper::toHtml('') );
 

nette/tracy

src/Tracy/Dumper.php
@@ -143,8 +143,10 @@ class Dumper
 
 	private static function dumpDouble(& $var)
 	{
-		$var = json_encode($var);
-		return '<span class="tracy-dump-number">' . $var . (strpos($var, '.') === FALSE ? '.0' : '') . "</span>\n";
+		$var = is_finite($var)
+			? ($tmp = json_encode($var)) . (strpos($tmp, '.') === FALSE ? '.0' : '')
+			: var_export($var, TRUE);
+		return "<span class=\"tracy-dump-number\">$var</span>\n";
 	}
 
 

tests/Tracy/Dumper.toHtml().phpt
@@ -44,6 +44,15 @@ Assert::match( '<pre class="tracy-dump"><span class="tracy-dump-number">0.0</spa
 Assert::match( '<pre class="tracy-dump"><span class="tracy-dump-number">0.1</span>
 </pre>', Dumper::toHtml(0.1) );
 
+Assert::match( '<pre class="tracy-dump"><span class="tracy-dump-number">INF</span>
+</pre>', Dumper::toHtml(INF) );
+
+Assert::match( '<pre class="tracy-dump"><span class="tracy-dump-number">-INF</span>
+</pre>', Dumper::toHtml(-INF) );
+
+Assert::match( '<pre class="tracy-dump"><span class="tracy-dump-number">NAN</span>
+</pre>', Dumper::toHtml(NAN) );
+
 Assert::match( '<pre class="tracy-dump"><span class="tracy-dump-string">""</span>
 </pre>', Dumper::toHtml('') );
 