82cdc688502940a283bd68c59a54e6eae80cf893
David Grudl <david@grudl.com>
2014-03-10 15:02:50 +0100

    removed support for IE < 9

--- src/Tracy/templates/bluescreen.js
+++ src/Tracy/templates/bluescreen.js
@@ -8,25 +8,23 @@
 (function(){
 	var bs = document.getElementById('tracyBluescreen');
 	document.body.appendChild(bs);
-	document.onkeyup = function(e) {
-		e = e || window.event;
+
+	document.addEventListener('keyup', function(e) {
 		if (e.keyCode == 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) {
 			document.getElementById('tracyBluescreenIcon').click();
 		}
-	};
+	});
 
 	for (var i = 0, styles = []; i < document.styleSheets.length; i++) {
 		var style = document.styleSheets[i];
-		if ((style.owningElement || style.ownerNode).className !== 'tracy-debug') {
+		if (style.ownerNode.className !== 'tracy-debug') {
 			style.oldDisabled = style.disabled;
 			style.disabled = true;
 			styles.push(style);
 		}
 	}
 
-	bs.onclick = function(e) {
-		e = e || window.event;
-
+	bs.addEventListener('click', function(e) {
 		if (e.ctrlKey) {
 			for (var link = e.target; link && (!link.getAttribute || !link.getAttribute('data-tracy-href')); link = link.parentNode) {}
 			if (link) {
@@ -39,30 +37,24 @@
 			return;
 		}
 
-		for (var link = e.target || e.srcElement; link && (!link.tagName || !link.className.match(/(^|\s)tracy-toggle(\s|$)/)); link = link.parentNode) {}
+		for (var link = e.target; link && (!link.tagName || !link.className.match(/(^|\s)tracy-toggle(\s|$)/)); link = link.parentNode) {}
 		if (!link) {
 			return true;
 		}
 
 		var collapsed = link.className.match(/(^|\s)tracy-collapsed(\s|$)/),
 			ref = link.getAttribute('data-ref') || link.getAttribute('href', 2),
-			dest;
+			dest = ref && ref !== '#' ? document.getElementById(ref.substring(1)) : link.nextElementSibling;
 
-		if (ref && ref !== '#') {
-			dest = document.getElementById(ref.substring(1));
-		} else {
-			for (dest = link.nextSibling; dest && dest.nodeType !== 1; dest = dest.nextSibling) {}
-		}
-
-		link.className = (collapsed ? link.className.replace(/(^|\s)tracy-collapsed(\s|$)/, ' ') : link.className + ' tracy-collapsed').replace(/^\s+|\s+$/g,'');
-		dest.className = (collapsed ? dest.className.replace(/(^|\s)tracy-collapsed(\s|$)/, ' ') : dest.className + ' tracy-collapsed').replace(/^\s+|\s+$/g,'');
+		link.className = (link.className.replace(/(^|\s)tracy-collapsed(\s|$)/, ' ') + (collapsed ? '' : ' tracy-collapsed')).trim();
+		dest.className = (dest.className.replace(/(^|\s)tracy-collapsed(\s|$)/, ' ') + (collapsed ? '' : ' tracy-collapsed')).trim();
 
 		if (link.id === 'tracyBluescreenIcon') {
 			for (i = 0; i < styles.length; i++) {
 				styles[i].disabled = collapsed ? true : styles[i].oldDisabled;
 			}
 		}
-		e.preventDefault ? e.preventDefault() : e.returnValue = false;
-		e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
-	};
+		e.preventDefault();
+		e.stopPropagation();
+	});
 })();
