nette/nette

Dumper: JS for nette-toggle doesn't collide with SVG

Nette/Diagnostics/templates/dumper.js
@@ -27,7 +27,7 @@
 			}
 
 			// enables <a class="nette-toggle" href="#"> or <span data-ref="#"> toggling
-			for (link = e.target; link && (!link.tagName || link.className.indexOf('nette-toggle') < 0); link = link.parentNode) {}
+			for (link = e.target; link && (!link.tagName || typeof link.className !== 'string' || link.className.indexOf('nette-toggle') < 0); link = link.parentNode) {}
 			if (!link) {
 				return;
 			}

nette/tracy

src/Tracy/templates/dumper.js
@@ -27,7 +27,7 @@
 			}
 
 			// enables <a class="tracy-toggle" href="#"> or <span data-ref="#"> toggling
-			for (link = e.target; link && (!link.tagName || link.className.indexOf('tracy-toggle') < 0); link = link.parentNode) {}
+			for (link = e.target; link && (!link.tagName || typeof link.className !== 'string' || link.className.indexOf('tracy-toggle') < 0); link = link.parentNode) {}
 			if (!link) {
 				return;
 			}