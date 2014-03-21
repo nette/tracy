nette/nette

Debugger: collapsing if done via class instead of display:none

Nette/Diagnostics/templates/dumper.js
@@ -38,7 +38,7 @@
 				oldPosition = panel.position();
 
 			link.className = 'nette-toggle' + (collapsed ? '' : '-collapsed');
-			dest[collapsed ? 'show' : 'hide']();
+			dest[collapsed ? 'removeClass' : 'addClass']('nette-collapsed');
 			e.preventDefault();
 
 			if (panel.length) {

nette/tracy

src/Tracy/templates/dumper.js
@@ -38,7 +38,7 @@
 				oldPosition = panel.position();
 
 			link.className = 'tracy-toggle' + (collapsed ? '' : '-collapsed');
-			dest[collapsed ? 'show' : 'hide']();
+			dest[collapsed ? 'removeClass' : 'addClass']('tracy-collapsed');
 			e.preventDefault();
 
 			if (panel.length) {