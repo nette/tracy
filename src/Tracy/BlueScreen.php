30a7be5e6bf96776775d88c1b11bdf7edb014f2a
David Grudl <david@grudl.com>
2014-05-20 02:38:48 +0200

    BlueScreen: dumping variables in attribute 'title' works with NULLs and dump is trimmed

--- src/Tracy/BlueScreen.php
+++ src/Tracy/BlueScreen.php
@@ -61,7 +61,7 @@ class BlueScreen
 	 * @param  int
 	 * @return string
 	 */
-	public static function highlightFile($file, $line, $lines = 15, $vars = array())
+	public static function highlightFile($file, $line, $lines = 15, array $vars = NULL)
 	{
 		$source = @file_get_contents($file); // intentionally @
 		if ($source) {
@@ -81,7 +81,7 @@ class BlueScreen
 	 * @param  int
 	 * @return string
 	 */
-	public static function highlightPhp($source, $line, $lines = 15, $vars = array())
+	public static function highlightPhp($source, $line, $lines = 15, array $vars = NULL)
 	{
 		if (function_exists('ini_set')) {
 			ini_set('highlight.comment', '#998; font-style: italic');
@@ -95,13 +95,15 @@ class BlueScreen
 		$source = explode("\n", highlight_string($source, TRUE));
 		$out = $source[0]; // <code><span color=highlight.html>
 		$source = str_replace('<br />', "\n", $source[1]);
-
 		$out .= static::highlightLine($source, $line, $lines);
-		$out = preg_replace_callback('#">\$(\w+)(&nbsp;)?</span>#', function($m) use ($vars) {
-			return isset($vars[$m[1]])
-				? '" title="' . str_replace('"', '&quot;', strip_tags(Dumper::toHtml($vars[$m[1]]))) . $m[0]
-				: $m[0];
-		}, $out);
+
+		if ($vars) {
+			$out = preg_replace_callback('#">\$(\w+)(&nbsp;)?</span>#', function($m) use ($vars) {
+				return array_key_exists($m[1], $vars)
+					? '" title="' . str_replace('"', '&quot;', trim(strip_tags(Dumper::toHtml($vars[$m[1]])))) . $m[0]
+					: $m[0];
+			}, $out);
+		}
 
 		return "<pre class='php'><div>$out</div></pre>";
 	}
