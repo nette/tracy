c5304e7f28675a9510a98d1f133800ea08b9a313
David Grudl <david@grudl.com>
2014-06-16 17:17:46 +0200

    fixed in_array

--- src/Tracy/Debugger.php
+++ src/Tracy/Debugger.php
@@ -375,7 +375,7 @@ class Debugger
 		}
 
 		$error = error_get_last();
-		if (in_array($error['type'], array(E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE))) {
+		if (in_array($error['type'], array(E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE), TRUE)) {
 			self::_exceptionHandler(Helpers::fixStack(new ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line'])), FALSE);
 
 		} elseif (!connection_aborted() && !self::$productionMode && self::isHtmlMode()) {
