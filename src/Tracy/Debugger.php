f5a2647c9d0174d218d626eab3952ea3a523c6e7
David Grudl <david@grudl.com>
2014-06-24 03:18:03 +0200

    Released version 2.2.2

--- src/Tracy/Debugger.php
+++ src/Tracy/Debugger.php
@@ -23,7 +23,7 @@ use Tracy,
 class Debugger
 {
 	/** @var string */
-	public static $version = '2.2.1';
+	public static $version = '2.2.2';
 
 	/** @var bool in production mode is suppressed any debugging output */
 	public static $productionMode = self::DETECT;
