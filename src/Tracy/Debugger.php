15b8d1484878d70d7e8dcb3fcb251acd87a8b2b1
David Grudl <david@grudl.com>
2014-07-09 13:10:36 +0200

    Debugger: shutdownHandler process E_RECOVERABLE_ERROR & E_USER_ERROR

--- src/Tracy/Debugger.php
+++ src/Tracy/Debugger.php
@@ -191,7 +191,7 @@ class Debugger
 		}
 
 		$error = error_get_last();
-		if (in_array($error['type'], array(E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE), TRUE)) {
+		if (in_array($error['type'], array(E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE, E_RECOVERABLE_ERROR, E_USER_ERROR), TRUE)) {
 			self::exceptionHandler(Helpers::fixStack(new ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line'])), FALSE);
 
 		} elseif (!connection_aborted() && !self::$productionMode && self::isHtmlMode()) {
