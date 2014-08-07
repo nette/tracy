8b27ad1ad1a0734748800d5ff0a13b82a8526f77
Patrik Votocek <patrik@votocek.cz>
2014-08-07 18:18:56 +0200

    Define severity for logging whole bluescreen in production mode [Closes #56]

--- src/Tracy/Debugger.php
+++ src/Tracy/Debugger.php
@@ -59,6 +59,9 @@ class Debugger
 	/** @var string name of the directory where errors should be logged */
 	public static $logDirectory;
 
+	/** @var int  log bluescreen in production mode for this error severity */
+	public static $logSeverity = 0;
+
 	/** @var string|array email(s) to which send error notifications */
 	public static $email;
 
@@ -296,6 +299,12 @@ class Debugger
 
 		} elseif (($severity & error_reporting()) !== $severity) {
 			return FALSE; // calls normal error handler to fill-in error_get_last()
+
+		} elseif (($severity & self::$logSeverity) === $severity) {
+			$e = new ErrorException($message, 0, $severity, $file, $line);
+			$e->context = $context;
+			self::log($e, self::ERROR);
+			return NULL;
 
 		} elseif (!self::$productionMode && !isset($_GET['_tracy_skip_error'])
 			&& (is_bool(self::$strictMode) ? self::$strictMode : ((self::$strictMode & $severity) === $severity))
