27a7d16c482fd1105cd65e3d40f199e78c3f7d74
David Grudl <david@grudl.com>
2014-08-31 14:41:40 +0200

    bluescreen.js uses dumper.js

--- src/Tracy/templates/bluescreen.js
+++ src/Tracy/templates/bluescreen.js
@@ -1,19 +1,9 @@
 /**
- * Bluescreen.
- *
  * This file is part of the Tracy (http://tracy.nette.org)
- * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
  */
 
 (function(){
-	var bs = document.getElementById('tracyBluescreen');
-	document.body.appendChild(bs);
-
-	document.addEventListener('keyup', function(e) {
-		if (e.keyCode == 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) {
-			document.getElementById('tracyBluescreenIcon').click();
-		}
-	});
+	document.body.appendChild(document.getElementById('tracyBluescreen'));
 
 	for (var i = 0, styles = []; i < document.styleSheets.length; i++) {
 		var style = document.styleSheets[i];
@@ -24,37 +14,16 @@
 		}
 	}
 
-	bs.addEventListener('click', function(e) {
-		if (e.ctrlKey) {
-			for (var link = e.target; link && (!link.getAttribute || !link.getAttribute('data-tracy-href')); link = link.parentNode) {}
-			if (link) {
-				location.href = link.getAttribute('data-tracy-href');
-				return false;
-			}
+	document.getElementById('tracyBluescreenIcon').addEventListener('click', function(e) {
+		var collapsed = Tracy.hasClass(this, 'tracy-collapsed');
+		for (i = 0; i < styles.length; i++) {
+			styles[i].disabled = collapsed ? true : styles[i].oldDisabled;
 		}
+	});
 
-		if (e.shiftKey || e.altKey || e.ctrlKey || e.metaKey) {
-			return;
+	document.addEventListener('keyup', function(e) {
+		if (e.keyCode === 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) { // ESC
+			document.getElementById('tracyBluescreenIcon').click();
 		}
-
-		for (var link = e.target; link && (!link.tagName || !Tracy.hasClass(link, 'tracy-toggle')); link = link.parentNode) {}
-		if (!link) {
-			return true;
-		}
-
-		var collapsed = Tracy.hasClass(link, 'tracy-collapsed'),
-			ref = link.getAttribute('data-ref') || link.getAttribute('href', 2),
-			dest = ref && ref !== '#' ? document.getElementById(ref.substring(1)) : link.nextElementSibling;
-
-		Tracy[collapsed ? 'removeClass' : 'addClass'](link, 'tracy-collapsed');
-		Tracy[collapsed ? 'removeClass' : 'addClass'](dest, 'tracy-collapsed');
-
-		if (link.id === 'tracyBluescreenIcon') {
-			for (i = 0; i < styles.length; i++) {
-				styles[i].disabled = collapsed ? true : styles[i].oldDisabled;
-			}
-		}
-		e.preventDefault();
-		e.stopPropagation();
 	});
 })();
