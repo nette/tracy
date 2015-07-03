5032f95676c717b459a293bf3fa0ce62a5804246
David Grudl <david@grudl.com>
2015-07-03 13:14:49 +0200

    examples: Tracy is enabled before output is print; added redirect example

--- examples/barDump.php
+++ examples/barDump.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html><link rel="stylesheet" href="assets/style.css">
-
-<style> html { background: url(assets/arrow.png) no-repeat bottom right; height: 100%; } </style>
-
-<h1>Tracy Debug Bar demo</h1>
-
-<p>You can dump variables to bar in rightmost bottom egde.</p>
-
 <?php
 
 require __DIR__ . '/../src/tracy.php';
@@ -14,8 +6,15 @@ use Tracy\Debugger;
 
 Debugger::enable(Debugger::DEVELOPMENT, __DIR__ . '/log');
 
-$arr = array(10, 20.2, TRUE, NULL, 'hello', (object) NULL, array());
+?>
+<!DOCTYPE html><html class=arrow><link rel="stylesheet" href="assets/style.css">
 
+<h1>Tracy: bar dump demo</h1>
+
+<p>You can dump variables to bar in rightmost bottom egde.</p>
+
+<?php
+$arr = array(10, 20.2, TRUE, NULL, 'hello', (object) NULL, array());
 
 Debugger::barDump(get_defined_vars());
 
