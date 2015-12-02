78e20852253cbc6fe5c832fa63759ae09d2743a6
David Grudl <david@grudl.com>
2015-12-02 16:32:29 +0100

    Throwable replaces Exception

--- src/Tracy/FireLogger.php
+++ src/Tracy/FireLogger.php
@@ -52,7 +52,7 @@ class FireLogger implements ILogger
 			$item['template'] = array_shift($args);
 		}
 
-		if (isset($args[0]) && ($args[0] instanceof \Exception || $args[0] instanceof \Throwable)) {
+		if (isset($args[0]) && $args[0] instanceof \Throwable) {
 			$e = array_shift($args);
 			$trace = $e->getTrace();
 			if (
