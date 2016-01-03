87288392ba11879dca33447c591f5939765fdbe6
Patrik Votocek <patrik@votocek.cz>
2016-01-03 03:26:52 +0100

    BlueScreen: add renderToFile

--- src/Tracy/Logger.php
+++ src/Tracy/Logger.php
@@ -141,15 +141,8 @@ class Logger implements ILogger
 	protected function logException($exception, $file = NULL)
 	{
 		$file = $file ?: $this->getExceptionFile($exception);
-		if ($handle = @fopen($file, 'x')) { // @ file may already exist
-			ob_start(); // double buffer prevents sending HTTP headers in some PHP
-			ob_start(function ($buffer) use ($handle) { fwrite($handle, $buffer); }, 4096);
-			$bs = $this->blueScreen ?: new BlueScreen;
-			$bs->render($exception);
-			ob_end_flush();
-			ob_end_clean();
-			fclose($handle);
-		}
+		$bs = $this->blueScreen ?: new BlueScreen;
+		$bs->renderToFile($exception, $file);
 		return $file;
 	}
 
