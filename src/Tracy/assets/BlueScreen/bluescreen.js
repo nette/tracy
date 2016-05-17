216d9eff29e3684c4b2ba3380f1e20fb41f1265d
David Grudl <david@grudl.com>
2016-05-17 16:01:09 +0200

    bluescreen.js: initialization wrapped in Tracy.BlueScreen.init()

--- src/Tracy/assets/BlueScreen/bluescreen.js
+++ src/Tracy/assets/BlueScreen/bluescreen.js
@@ -3,32 +3,45 @@
  */
 
 (function(){
-	var blueScreen = document.getElementById('tracy-bs');
-	document.body.appendChild(blueScreen);
+	Tracy = window.Tracy || {};
 
-	for (var i = 0, styles = []; i < document.styleSheets.length; i++) {
-		var style = document.styleSheets[i];
-		if (!style.ownerNode.classList.contains('tracy-debug')) {
-			style.oldDisabled = style.disabled;
-			style.disabled = true;
-			styles.push(style);
+	var BlueScreen = Tracy.BlueScreen = {},
+		inited;
+
+	BlueScreen.init = function() {
+		var blueScreen = document.getElementById('tracy-bs');
+		document.body.appendChild(blueScreen);
+
+		for (var i = 0, styles = []; i < document.styleSheets.length; i++) {
+			var style = document.styleSheets[i];
+			if (!style.ownerNode.classList.contains('tracy-debug')) {
+				style.oldDisabled = style.disabled;
+				style.disabled = true;
+				styles.push(style);
+			}
 		}
+
+		document.getElementById('tracy-bs-toggle').addEventListener('tracy-toggle', function(e) {
+			var collapsed = this.classList.contains('tracy-collapsed');
+			for (i = 0; i < styles.length; i++) {
+				styles[i].disabled = collapsed ? styles[i].oldDisabled : true;
+			}
+		});
+
+		var id = location.href + document.getElementById('tracy-bs-error').textContent;
+		Tracy.Toggle.persist(blueScreen, sessionStorage.getItem('tracy-toggles-bskey') === id);
+		sessionStorage.setItem('tracy-toggles-bskey', id);
+
+		if (inited) {
+			return;
+		}
+		inited = true;
+
+		// enables toggling via ESC
+		document.addEventListener('keyup', function(e) {
+			if (e.keyCode === 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) { // ESC
+				Tracy.Toggle.toggle(document.getElementById('tracy-bs-toggle'));
+			}
+		});
 	}
-
-	document.getElementById('tracy-bs-toggle').addEventListener('tracy-toggle', function(e) {
-		var collapsed = this.classList.contains('tracy-collapsed');
-		for (i = 0; i < styles.length; i++) {
-			styles[i].disabled = collapsed ? styles[i].oldDisabled : true;
-		}
-	});
-
-	document.addEventListener('keyup', function(e) {
-		if (e.keyCode === 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) { // ESC
-			Tracy.Toggle.toggle(document.getElementById('tracy-bs-toggle'));
-		}
-	});
-
-	var id = location.href + document.getElementById('tracy-bs-error').textContent;
-	Tracy.Toggle.persist(blueScreen, sessionStorage.getItem('tracy-toggles-bskey') === id);
-	sessionStorage.setItem('tracy-toggles-bskey', id);
 })();
