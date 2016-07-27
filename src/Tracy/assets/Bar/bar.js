f53a1b841f3d4d577982b902b6c80b07c7795831
wppd <luan.bu@gmail.com>
2016-07-27 20:04:40 +0800

    bar.js: fix for protocol-less ajax url request (#211)

--- src/Tracy/assets/Bar/bar.js
+++ src/Tracy/assets/Bar/bar.js
@@ -390,7 +390,7 @@
 
 		XMLHttpRequest.prototype.open = function() {
 			oldOpen.apply(this, arguments);
-			if (window.TracyAutoRefresh !== false && arguments[1].indexOf('//') < 0 || arguments[1].indexOf(location.origin + '/') === 0) {
+			if (window.TracyAutoRefresh !== false && arguments[1].indexOf('//') <= 0 || arguments[1].indexOf(location.origin + '/') === 0) {
 				this.setRequestHeader('X-Tracy-Ajax', header);
 			}
 		};
