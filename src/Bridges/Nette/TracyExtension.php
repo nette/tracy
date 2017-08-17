74d4171b9d79388acc6400cb5edf0a12dd95cc6c
David Grudl <david@grudl.com>
2017-08-17 14:34:19 +0200

    TracyExtension: use is_writable()

--- src/Bridges/Nette/TracyExtension.php
+++ src/Bridges/Nette/TracyExtension.php
@@ -118,18 +118,8 @@ class TracyExtension extends Nette\DI\CompilerExtension
 			));
 		}
 
-		if ($dir = Tracy\Debugger::$logDirectory) {
-			$this->checkLogDirectory($dir);
+		if (($dir = Tracy\Debugger::$logDirectory) && !is_writable($dir)) {
+			throw new Nette\InvalidStateException("Make directory '$dir' writable.");
 		}
-	}
-
-
-	private function checkLogDirectory($dir)
-	{
-		$uniq = uniqid('_', true);
-		if (!@mkdir("$dir/$uniq")) { // @ - is escalated to exception
-			throw new Nette\InvalidStateException("Unable to write to directory '$dir'. Make this directory writable.");
-		}
-		rmdir("$dir/$uniq");
 	}
 }
