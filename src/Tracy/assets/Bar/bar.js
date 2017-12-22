d7cf17a337ec157eddb0e4ec1f871cc00d341d91
adrianbj <adrianbjones@gmail.com>
2017-12-22 10:50:37 -0800

    Allow adding of custom icons to panel header. By being explicit about what the "window" icon does, we can add others without them triggering this action.

--- src/Tracy/assets/Bar/bar.js
+++ src/Tracy/assets/Bar/bar.js
@@ -71,10 +71,9 @@
 
 		forEach(elem.querySelectorAll('.tracy-icons a'), function(a) {
 			a.addEventListener('click', function(e) {
-				clearTimeout(elem.Tracy.displayTimeout);
 				if (this.rel === 'close') {
 					_this.toPeek();
-				} else {
+				} else if (this.rel === 'window') {
 					_this.toWindow();
 				}
 				e.preventDefault();
