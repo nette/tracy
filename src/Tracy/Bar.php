36a694f69795307c32daec1549d0ac845bc54aba
Jakub Bouček <pan@jakubboucek.cz>
2018-10-03 13:56:07 +0200

    bar: send with Content-type: application/javascript (#322)

--- src/Tracy/Bar.php
+++ src/Tracy/Bar.php
@@ -174,7 +174,7 @@ class Bar
 	{
 		$asset = $_GET['_tracy_bar'] ?? null;
 		if ($asset === 'js') {
-			header('Content-Type: text/javascript');
+			header('Content-Type: application/javascript');
 			header('Cache-Control: max-age=864000');
 			header_remove('Pragma');
 			header_remove('Set-Cookie');
@@ -190,7 +190,7 @@ class Bar
 
 		if ($this->useSession && $asset && preg_match('#^content(-ajax)?\.(\w+)$#', $asset, $m)) {
 			$session = &$_SESSION['_tracy']['bar'][$m[2] . $m[1]];
-			header('Content-Type: text/javascript');
+			header('Content-Type: application/javascript');
 			header('Cache-Control: max-age=60');
 			header_remove('Set-Cookie');
 			if (!$m[1]) {
