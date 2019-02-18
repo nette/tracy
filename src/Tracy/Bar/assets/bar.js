ffe92ae44df2ad56de32319e3e87e46a4382213f
David Grudl <david@grudl.com>
2019-02-18 16:07:58 +0100

    bar: replaced attribute 'rel' with 'data-tracy-action' for closing & open-to-window links

--- src/Tracy/Bar/assets/bar.js
+++ src/Tracy/Bar/assets/bar.js
@@ -60,9 +60,9 @@
 
 			elem.querySelectorAll('.tracy-icons a').forEach((link) => {
 				link.addEventListener('click', (e) => {
-					if (link.rel === 'close') {
+					if (link.dataset.tracyAction === 'close') {
 						this.toPeek();
-					} else if (link.rel === 'window') {
+					} else if (link.dataset.tracyAction === 'window') {
 						this.toWindow();
 					}
 					e.preventDefault();
@@ -254,7 +254,7 @@
 		initTabs(elem) {
 			elem.querySelectorAll('a').forEach((link) => {
 				link.addEventListener('click', (e) => {
-					if (link.rel === 'close') {
+					if (link.dataset.tracyAction === 'close') {
 						this.close();
 
 					} else if (link.rel) {
@@ -280,7 +280,7 @@
 				});
 
 				link.addEventListener('mouseenter', (e) => {
-					if (e.buttons || !link.rel || link.rel === 'close' || elem.classList.contains('tracy-dragged')) {
+					if (e.buttons || !link.rel || elem.classList.contains('tracy-dragged')) {
 						return;
 					}
 
@@ -307,7 +307,7 @@
 				link.addEventListener('mouseleave', () => {
 					clearTimeout(this.displayTimeout);
 
-					if (link.rel && link.rel !== 'close' && !elem.classList.contains('tracy-dragged')) {
+					if (link.rel && !elem.classList.contains('tracy-dragged')) {
 						Debug.panels[link.rel].blur();
 					}
 				});
