4018fa66ce6fdd857a927bdf7abb0f84b293c3bf
David Grudl <david@grudl.com>
2019-02-22 16:35:38 +0100

    TableSort: refactoring

--- src/Tracy/TableSort/table-sort.js
+++ src/Tracy/TableSort/table-sort.js
@@ -6,29 +6,32 @@
 
 (function() {
 
+	// enables <table class="tracy-sortable">
 	class TableSort
 	{
 		static init(context) {
 			context.addEventListener('click', (e) => {
-				if (!e.target.matches('tr:first-child *')) {
-					return;
+				if (e.target.matches('tr:first-child *')) {
+					TableSort.sort(e.target.closest('td,th'));
 				}
-				let tcell = e.target.closest('td,th');
-				let tbody = tcell.closest('table').tBodies[0];
-				let preserveFirst = !tcell.closest('thead') && !tcell.parentNode.querySelectorAll('td').length;
-				let asc = !(tbody.tracyAsc === tcell.cellIndex);
-				tbody.tracyAsc = asc ? tcell.cellIndex : null;
-				let getText = (cell) => { return cell ? cell.innerText : ''; };
-
-				Array.from(tbody.querySelectorAll('tr'))
-					.slice(preserveFirst ? 1 : 0)
-					.sort((a, b) => {
-						return function(v1, v2) {
-							return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
-						}(getText((asc ? a : b).children[tcell.cellIndex]), getText((asc ? b : a).children[tcell.cellIndex]));
-					})
-					.forEach((tr) => { tbody.appendChild(tr); });
 			});
+		}
+
+		static sort(tcell) {
+			let tbody = tcell.closest('table').tBodies[0];
+			let preserveFirst = !tcell.closest('thead') && !tcell.parentNode.querySelectorAll('td').length;
+			let asc = !(tbody.tracyAsc === tcell.cellIndex);
+			tbody.tracyAsc = asc ? tcell.cellIndex : null;
+			let getText = (cell) => { return cell ? cell.innerText : ''; };
+
+			Array.from(tbody.querySelectorAll('tr'))
+				.slice(preserveFirst ? 1 : 0)
+				.sort((a, b) => {
+					return function(v1, v2) {
+						return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
+					}(getText((asc ? a : b).children[tcell.cellIndex]), getText((asc ? b : a).children[tcell.cellIndex]));
+				})
+				.forEach((tr) => { tbody.appendChild(tr); });
 		}
 	}
 
