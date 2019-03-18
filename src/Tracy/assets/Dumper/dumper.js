45994a06f253ef611d2c64344a623a1b0a193a0c
adrianbj <adrianbjones@gmail.com>
2019-03-18 12:15:35 -0700

    dumper.js: fixed insertChild is not a function error

--- src/Tracy/assets/Dumper/dumper.js
+++ src/Tracy/assets/Dumper/dumper.js
@@ -14,7 +14,7 @@
 				[].forEach.call((context || document).querySelectorAll('.tracy-dump[data-tracy-dump]'), (el) => {
 					try {
 						let built = build(JSON.parse(el.getAttribute('data-tracy-dump')), repository, el.classList.contains('tracy-collapsed'));
-						el.insertChild(built, el.lastChild);
+						el.insertBefore(built, el.lastChild);
 						el.classList.remove('tracy-collapsed');
 						el.removeAttribute('data-tracy-dump');
 					} catch (e) {
