54ec37a25654c53119559eeba7de21b24d8a2889
David Grudl <david@grudl.com>
2019-09-24 22:10:17 +0200

    Debugger::enable() preloading via class_exists changed to require_once [Closes #389]

--- src/Tracy/Debugger/Debugger.php
+++ src/Tracy/Debugger/Debugger.php
@@ -199,8 +199,9 @@ class Debugger
 		set_exception_handler([__CLASS__, 'exceptionHandler']);
 		set_error_handler([__CLASS__, 'errorHandler']);
 
-		array_map('class_exists', [Bar::class, BlueScreen::class, DefaultBarPanel::class, Dumper::class,
-			FireLogger::class, Helpers::class, Logger::class, ]);
+		foreach (['Bar/Bar', 'Bar/DefaultBarPanel', 'BlueScreen/BlueScreen', 'Dumper/Dumper', 'Logger/Logger', 'Helpers'] as $path) {
+			require_once dirname(__DIR__) . "/$path.php";
+		}
 
 		self::dispatch();
 		self::$enabled = true;
