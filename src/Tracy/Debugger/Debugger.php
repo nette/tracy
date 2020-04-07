f0e3e21ddae681db81f81bacd7165b0acd752b46
Jan Barášek <janbarasek@gmail.com>
2020-04-07 15:45:49 +0200

    Debugger::enable(): More helpful comment

--- src/Tracy/Debugger/Debugger.php
+++ src/Tracy/Debugger/Debugger.php
@@ -145,7 +145,7 @@ class Debugger
 
 	/**
 	 * Enables displaying or logging errors and exceptions.
-	 * @param  mixed   $mode  production, development mode, autodetection or IP address(es) whitelist.
+	 * @param  bool|string|string[]  $mode  true => production, false => development mode, autodetection or IP address(es) whitelist.
 	 * @param  string  $logDirectory  error log directory
 	 * @param  string|array  $email  administrator email; enables email sending in production mode
 	 */
