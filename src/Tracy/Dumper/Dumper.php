e81fe89796c3cfe3eabb5f1b9b714f9484057e03
David Grudl <david@grudl.com>
2020-04-11 05:40:09 +0200

    uses PHP 7.2 features
    - keys in objects are always strings
    - object typehint

--- src/Tracy/Dumper/Dumper.php
+++ src/Tracy/Dumper/Dumper.php
@@ -158,9 +158,7 @@ class Dumper
 		if ($options[self::LIVE] ?? false) {
 			$this->snapshot = &self::$liveSnapshot;
 		}
-		$this->lazy = is_array($this->snapshot)
-			? true
-			: ($options[self::LAZY] ?? $this->lazy);
+		$this->lazy = is_array($this->snapshot) ? true : ($options[self::LAZY] ?? $this->lazy);
 		$this->debugInfo = $options[self::DEBUGINFO] ?? $this->debugInfo;
 		$this->keysToHide = array_flip(array_map('strtolower', $options[self::KEYS_TO_HIDE] ?? []));
 		$this->resourceDumpers = ($options['resourceExporters'] ?? []) + self::$resources;
@@ -233,9 +231,7 @@ class Dumper
 	 */
 	private function dumpVar(&$var, array $options, int $level = 0): string
 	{
-		if (!method_exists(self::class, $m = 'dump' . explode(' ', gettype($var))[0])) {
-			$m = 'dumpResource'; // closed resource is 'unknown type' in PHP 7.1
-		}
+		$m = 'dump' . explode(' ', gettype($var))[0];
 		return $this->$m($var, $options, $level);
 	}
 
@@ -338,12 +334,8 @@ class Dumper
 
 		$editorAttributes = '';
 		if ($this->location & self::LOCATION_CLASS) {
-			$rc = $var instanceof \Closure
-				? new \ReflectionFunction($var)
-				: new \ReflectionClass($var);
-			$editor = $rc->getFileName()
-				? Helpers::editorUri($rc->getFileName(), $rc->getStartLine())
-				: null;
+			$rc = $var instanceof \Closure ? new \ReflectionFunction($var) : new \ReflectionClass($var);
+			$editor = $rc->getFileName() ? Helpers::editorUri($rc->getFileName(), $rc->getStartLine()) : null;
 			if ($editor) {
 				$editorAttributes = Helpers::formatHtml(
 					' title="Declared in file % on line %" data-tracy-href="%"',
@@ -379,12 +371,13 @@ class Dumper
 				$out = $span . '>' . $out . "</span>\n" . '<div' . ($collapsed ? ' class="tracy-collapsed"' : '') . '>';
 				$options['parents'][] = $var;
 				foreach ($fields as $k => &$v) {
+					$k = (string) $k;
 					$vis = '';
 					if (isset($k[0]) && $k[0] === "\x00") {
 						$vis = ' <span class="tracy-dump-visibility">' . ($k[1] === '*' ? 'protected' : 'private') . '</span>';
 						$k = substr($k, strrpos($k, "\x00") + 1);
 					}
-					$hide = is_string($k) && isset($this->keysToHide[strtolower($k)]);
+					$hide = isset($this->keysToHide[strtolower($k)]);
 					$out .= '<span class="tracy-dump-indent">   ' . str_repeat('|  ', $level) . '</span>'
 						. '<span class="tracy-dump-key">' . Helpers::escapeHtml($this->encodeKey($k)) . "</span>$vis => "
 						. (
@@ -476,9 +469,7 @@ class Dumper
 				'object' => $var,
 			];
 			if (empty($obj['editor']) && ($this->location & self::LOCATION_CLASS)) {
-				$rc = $var instanceof \Closure
-					? new \ReflectionFunction($var)
-					: new \ReflectionClass($var);
+				$rc = $var instanceof \Closure ? new \ReflectionFunction($var) : new \ReflectionClass($var);
 				if ($editor = $rc->getFileName() ? Helpers::editorUri($rc->getFileName(), $rc->getStartLine()) : null) {
 					$obj['editor'] = ['file' => $rc->getFileName(), 'line' => $rc->getStartLine(), 'url' => $editor];
 				}
@@ -490,11 +481,12 @@ class Dumper
 
 				foreach ($this->exportObject($var) as $k => $v) {
 					$vis = 0;
+					$k = (string) $k;
 					if (isset($k[0]) && $k[0] === "\x00") {
 						$vis = $k[1] === '*' ? 1 : 2;
 						$k = substr($k, strrpos($k, "\x00") + 1);
 					}
-					$hide = is_string($k) && isset($this->keysToHide[strtolower($k)]);
+					$hide = isset($this->keysToHide[strtolower($k)]);
 					$obj['items'][] = [$this->encodeKey($k), $hide ? ['type' => self::hideValue($v)] : $this->toJson($v, $options, $level + 1), $vis];
 				}
 			}
@@ -593,10 +585,7 @@ class Dumper
 	}
 
 
-	/**
-	 * @param  object  $obj
-	 */
-	private function exportObject($obj): array
+	private function exportObject(object $obj): array
 	{
 		foreach ($this->objectDumpers as $type => $dumper) {
 			if (!$type || $obj instanceof $type) {
@@ -676,7 +665,7 @@ class Dumper
 	private static function findLocation(): ?array
 	{
 		foreach (debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS) as $item) {
-			if (isset($item['class']) && $item['class'] === self::class) {
+			if (isset($item['class']) && $item['class'] === __CLASS__) {
 				$location = $item;
 				continue;
 			} elseif (isset($item['function'])) {
@@ -684,10 +673,7 @@ class Dumper
 					$reflection = isset($item['class'])
 						? new \ReflectionMethod($item['class'], $item['function'])
 						: new \ReflectionFunction($item['function']);
-					if (
-						$reflection->isInternal()
-						|| preg_match('#\s@tracySkipLocation\s#', (string) $reflection->getDocComment())
-					) {
+					if ($reflection->isInternal() || preg_match('#\s@tracySkipLocation\s#', (string) $reflection->getDocComment())) {
 						$location = $item;
 						continue;
 					}
