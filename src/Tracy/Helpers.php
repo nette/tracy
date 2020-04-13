d8fded83e4cdf6344bf0d3c6af976d0e5befdef7
David Grudl <david@grudl.com>
2020-04-13 12:40:22 +0200

    Dumper: encodeString() & truncateString() moved to Helpers

--- src/Tracy/Helpers.php
+++ src/Tracy/Helpers.php
@@ -332,4 +332,52 @@ class Helpers
 			throw $e;
 		}
 	}
+
+
+	/** @internal */
+	public static function encodeString(string $s, int $maxLength = null): string
+	{
+		if ($maxLength) {
+			$s = self::truncateString($tmp = $s, $maxLength);
+			$shortened = $s !== $tmp;
+		}
+
+		if (preg_match('#[^\x09\x0A\x0D\x20-\x7E\xA0-\x{10FFFF}]#u', $s) || preg_last_error()) { // is binary?
+			static $table;
+			if ($table === null) {
+				foreach (array_merge(range("\x00", "\x1F"), range("\x7F", "\xFF")) as $ch) {
+					$table[$ch] = '\x' . str_pad(dechex(ord($ch)), 2, '0', STR_PAD_LEFT);
+				}
+				$table['\\'] = '\\\\';
+				$table["\r"] = '\r';
+				$table["\n"] = '\n';
+				$table["\t"] = '\t';
+			}
+
+			$s = strtr($s, $table);
+		}
+
+		return $s . (empty($shortened) ? '' : ' ... ');
+	}
+
+
+	/** @internal */
+	public static function truncateString(string $s, int $maxLength): string
+	{
+		if (!preg_match('##u', $s)) {
+			$s = substr($s, 0, $maxLength); // not UTF-8
+		} elseif (function_exists('mb_substr')) {
+			$s = mb_substr($s, 0, $maxLength, 'UTF-8');
+		} else {
+			$i = $len = 0;
+			while (isset($s[$i])) {
+				if (($s[$i] < "\x80" || $s[$i] >= "\xC0") && (++$len > $maxLength)) {
+					$s = substr($s, 0, $i);
+					break;
+				}
+				$i++;
+			}
+		}
+		return $s;
+	}
 }
---
+++ tests/Tracy/Helpers.truncateString(().phpt
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Test: Tracy\Helpers::truncateString()
+ */
+
+declare(strict_types=1);
+
+use Tester\Assert;
+use Tracy\Helpers;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+
+Assert::same('', Helpers::truncateString('', 1));
+Assert::same('h', Helpers::truncateString('hello', 1));
+Assert::same('hello', Helpers::truncateString('hello', 5));
+Assert::same('hello', Helpers::truncateString('hello', 6));
+
+Assert::same('Iñtërnâtiônàlizætiøn', Helpers::truncateString('Iñtërnâtiônàlizætiøn', 20));
+Assert::same('Iñtër', Helpers::truncateString('Iñtërnâtiônàlizætiøn', 5));
+
+Assert::same("\x00", Helpers::truncateString("\x00\x01", 1));
+Assert::same("\x00\x01", Helpers::truncateString("\x00\x01", 5));
+
+Assert::same('bad', Helpers::truncateString("bad\xff", 3));
+Assert::same("bad\xff", Helpers::truncateString("bad\xff", 4));
+Assert::same("bad\xff", Helpers::truncateString("bad\xff", 5));
