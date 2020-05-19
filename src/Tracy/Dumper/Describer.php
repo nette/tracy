2b5765a8c90cb8eafd54e8adb9ba5b9c6cbc6c8d
David Grudl <david@grudl.com>
2020-05-19 15:44:16 +0200

    Dumper: dumps zval references

--- src/Tracy/Dumper/Describer.php
+++ src/Tracy/Dumper/Describer.php
@@ -44,9 +44,13 @@ final class Describer
 	/** @var callable[] */
 	public $objectExposers;
 
+	/** @var int[] */
+	private $references = [];
+
 
 	public function describe(&$var): \stdClass
 	{
+		$this->references = [];
 		uksort($this->objectExposers, function ($a, $b): int {
 			return $b === '' || (class_exists($a, false) && is_subclass_of($a, $b)) ? -1 : 1;
 		});
@@ -118,14 +122,15 @@ final class Describer
 		$res = [];
 		try {
 			$arr[$marker] = true;
-			foreach ($arr as $k => &$v) {
+			foreach ($arr as $k => $v) {
 				if ($k !== $marker) {
+					$refId = $this->getReferenceId($arr, $k);
 					$res[] = [
 						$this->describeKey($k),
 							is_string($k) && isset($this->keysToHide[strtolower($k)])
 							? new Value('text', self::hideValue($v))
-							: $this->describeVar($v, $depth + 1),
-					];
+							: $this->describeVar($arr[$k], $depth + 1),
+					] + ($refId ? [2 => $refId] : []);
 				}
 			}
 		} finally {
@@ -157,8 +162,10 @@ final class Describer
 		if ($depth < $this->maxDepth) {
 			$value->items = [];
 
-			foreach ($this->exposeObject($obj) as $k => $v) {
+			$props = $this->exposeObject($obj);
+			foreach ($props as $k => $v) {
 				$type = 0;
+				$refId = $this->getReferenceId($props, $k);
 				$k = (string) $k;
 				if (isset($k[0]) && $k[0] === "\x00") {
 					$type = $k[1] === '*' ? 1 : 2;
@@ -167,7 +174,7 @@ final class Describer
 				$v = isset($this->keysToHide[strtolower($k)])
 					? new Value('text', self::hideValue($v))
 					: $this->describeVar($v, $depth + 1);
-				$value->items[] = [$this->describeKey($k), $v, $type];
+				$value->items[] = [$this->describeKey($k), $v, $type] + ($refId ? [3 => $refId] : []);
 			}
 		}
 		return new Value('ref', $id);
@@ -228,6 +235,35 @@ final class Describer
 	private static function hideValue($var): string
 	{
 		return self::HIDDEN_VALUE . ' (' . (is_object($var) ? Helpers::getClass($var) : gettype($var)) . ')';
+	}
+
+
+	private function getReferenceId($arr, $key): ?int
+	{
+		if (PHP_VERSION_ID >= 70400) {
+			if ((!$rr = \ReflectionReference::fromArrayElement($arr, $key))) {
+				return null;
+			}
+			$tmp = &$this->references[$rr->getId()];
+			if ($tmp === null) {
+				return $tmp = count($this->references);
+			}
+			return $tmp;
+		}
+		$uniq = new \stdClass;
+		$copy = $arr;
+		$orig = $copy[$key];
+		$copy[$key] = $uniq;
+		if ($arr[$key] !== $uniq) {
+			return null;
+		}
+		$res = array_search($uniq, $this->references, true);
+		$copy[$key] = $orig;
+		if ($res === false) {
+			$this->references[] = &$arr[$key];
+			return count($this->references);
+		}
+		return $res + 1;
 	}
 
 
