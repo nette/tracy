48d7ad6e31d0d070bb9c1d92111efc18e43ec026
David Grudl <david@grudl.com>
2020-12-28 01:13:21 +0100

    examples: improved dump() examples

--- examples/dump.php
+++ examples/dump.php
@@ -13,36 +13,85 @@ Debugger::enable(Debugger::DETECT, __DIR__ . '/log');
 ?>
 <!DOCTYPE html><link rel="stylesheet" href="assets/style.css">
 
-<h1>Tracy: Dumper demo</h1>
+<h1>Tracy: dump() demo</h1>
 
 <?php
 
-class Test
+
+echo "<h2>Basic Types</h2>\n";
+
+dump('any string', 123, [true, false, null]);
+
+
+
+echo "<h2>Dark Mode</h2>\n";
+
+Debugger::$dumpTheme = 'dark';
+
+dump('any string');
+
+
+
+echo "<h2>Objects</h2>\n";
+echo "<p>Hover over the name <code>\$baz</code> to see property declaring class and over the hash <code>#5</code> to see same objects.</p>\n";
+
+class ParentClass
 {
-	public $x = [10, null];
+	public $foo = [10, 20];
 
-	protected $z = 30;
+	protected $bar = 30;
 
-	private $y = 'hello';
+	private $baz = 'parent';
 }
 
-$arr = [10, 20.2, true, null, 'hello', (object) null, [], fopen(__FILE__, 'r')];
+class ChildClass extends ParentClass
+{
+	private $baz = 'child';
+}
 
-$obj = new Test;
 
-
-dump('<a href="#">test</a>');
-
-dump($arr);
+$obj = new ChildClass;
+$obj->dynamic = 'hello';
+$obj->selfReference = $obj;
 
 dump($obj);
 
 
-echo "<h2>With location</h2>\n";
 
-Debugger::$showLocation = true;
+echo "<h2>Strings</h2>\n";
+echo "<p>Hover over the string to see length.</p>\n";
+
+$arr = [
+	'single line' => 'hello',
+	'binary' => "binary\xA0string",
+	'multi line' => "first\r\nsecond\nthird\n   indented line",
+	'long' => str_repeat('tracy ', 1000), // affected by Tracy\Debugger::$maxLength
+];
 
 dump($arr);
+
+
+echo "<h2>References and Recursion</h2>\n";
+echo "<p>Hover over the reference <code>&1</code> to see referenced values.</p>\n";
+
+$arr = ['first', 'second', 'third'];
+$arr[] = &$arr[0];
+$arr[] = &$arr[1];
+$arr[] = &$arr;
+
+dump($arr);
+
+
+echo "<h2>Special Types</h2>\n";
+
+$arr = [
+	fopen(__FILE__, 'r'),
+	new class {},
+	function ($x, $y) use (&$arr, $obj) {},
+];
+
+dump($arr);
+
 
 
 if (Debugger::$productionMode) {
