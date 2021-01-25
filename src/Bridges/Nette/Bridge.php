853cb0690e45baae44dc7779fcfd5e1c0036493f
David Grudl <david@grudl.com>
2021-01-25 18:13:05 +0100

    strict fix [Closes #461]

--- src/Bridges/Nette/Bridge.php
+++ src/Bridges/Nette/Bridge.php
@@ -33,7 +33,7 @@ class Bridge
 
 	public static function renderLatteError(?\Throwable $e): ?array
 	{
-		if ($e instanceof Latte\CompileException) {
+		if ($e instanceof Latte\CompileException && $e->sourceName) {
 			return [
 				'tab' => 'Template',
 				'panel' => (preg_match('#\n|\?#', $e->sourceName)
@@ -70,6 +70,7 @@ class Bridge
 	{
 		if (
 			$e instanceof Latte\CompileException
+			&& $e->sourceName
 			&& @is_file($e->sourceName) // @ - may trigger error
 			&& (preg_match('#Unknown macro (\{\w+)\}, did you mean (\{\w+)\}\?#A', $e->getMessage(), $m)
 				|| preg_match('#Unknown attribute (n:\w+), did you mean (n:\w+)\?#A', $e->getMessage(), $m))
