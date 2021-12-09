bb5c4b86a5dfb20c2795a7a09cc4a5bdef57d41f
David Grudl <david@grudl.com>
2021-12-09 21:02:20 +0100

    Debugger::dispatch() is not starting session automatically

--- src/Tracy/Debugger/Debugger.php
+++ src/Tracy/Debugger/Debugger.php
@@ -252,16 +252,7 @@ class Debugger
 				. ($file ? "Output started at $file:$line." : 'Try Tracy\OutputDebugger to find where output started.')
 			);
 
-		} elseif (self::$enabled && session_status() !== PHP_SESSION_ACTIVE) {
-			ini_set('session.use_cookies', '1');
-			ini_set('session.use_only_cookies', '1');
-			ini_set('session.use_trans_sid', '0');
-			ini_set('session.cookie_path', '/');
-			ini_set('session.cookie_httponly', '1');
-			session_start();
-		}
-
-		if (self::getBar()->dispatchAssets()) {
+		} elseif (self::getBar()->dispatchAssets()) {
 			self::$showBar = false;
 			exit;
 		}
