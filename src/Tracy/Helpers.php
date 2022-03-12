edbd3883c97bb958a122ebe3a3116ca1628a0384
David Grudl <david@grudl.com>
2022-03-12 02:55:20 +0100

    Helpers: improved color detection
    - on Windows rely on sapi_windows_vt100_support()
    - on Linux rely on stream_isatty()
    - respects NO_COLOR and FORCE_COLOR

--- src/Tracy/Helpers.php
+++ src/Tracy/Helpers.php
@@ -571,17 +571,12 @@ XX
 
 	public static function detectColors(): bool
 	{
-		return (self::isCli())
+		return self::isCli()
 			&& getenv('NO_COLOR') === false // https://no-color.org
 			&& (getenv('FORCE_COLOR')
-				|| @stream_isatty(STDOUT) // @ may trigger error 'cannot cast a filtered stream on this system'
-				|| (defined('PHP_WINDOWS_VERSION_BUILD')
-					&& (function_exists('sapi_windows_vt100_support') && sapi_windows_vt100_support(STDOUT))
-						|| getenv('ConEmuANSI') === 'ON' // ConEmu
-						|| getenv('ANSICON') !== false // ANSICON
-						|| getenv('term') === 'xterm' // MSYS
-						|| getenv('term') === 'xterm-256color' // MSYS
-					)
+				|| (function_exists('sapi_windows_vt100_support')
+					? sapi_windows_vt100_support(STDOUT)
+					: @stream_isatty(STDOUT)) // @ may trigger error 'cannot cast a filtered stream on this system'
 			);
 	}
 
