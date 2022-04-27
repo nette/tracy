ed70898b43e5e64ede494afe713a8c2c2382685d
David Grudl <david@grudl.com>
2022-04-27 19:56:27 +0200

    coding style

--- src/Tracy/Dumper/Dumper.php
+++ src/Tracy/Dumper/Dumper.php
@@ -108,7 +108,7 @@ class Dumper
 			fwrite(STDOUT, $dumper->asTerminal($var, $useColors ? self::$terminalColors : []));
 
 		} elseif (Helpers::isHtmlMode()) {
-			$options[self::LOCATION] = $options[self::LOCATION] ?? true;
+			$options[self::LOCATION] ??= true;
 			self::renderAssets();
 			echo self::toHtml($var, $options);
 
@@ -196,8 +196,8 @@ class Dumper
 		}
 
 		if (isset($tmp)) {
-			$tmp[0] = $tmp[0] ?? [];
-			$tmp[1] = $tmp[1] ?? [];
+			$tmp[0] ??= [];
+			$tmp[1] ??= [];
 			$describer->snapshot = &$tmp[0];
 			$describer->references = &$tmp[1];
 		}
--- tests/Tracy/Dumper.toText().specials.enum.phpt
+++ tests/Tracy/Dumper.toText().specials.enum.phpt
@@ -27,11 +27,13 @@ enum Methods: string
 	case POST = 'post';
 }
 
-Assert::match(<<<'XX'
-array (3)
-   0 => Suit::Clubs #5
-   1 => Methods::GET #6
-   |  value: 'get'
-   2 => Methods::GET #6 see above
-XX
-, Dumper::toText([Suit::Clubs, Methods::GET, Methods::GET]));
+Assert::match(
+	<<<'XX'
+		array (3)
+		   0 => Suit::Clubs #5
+		   1 => Methods::GET #6
+		   |  value: 'get'
+		   2 => Methods::GET #6 see above
+		XX,
+	Dumper::toText([Suit::Clubs, Methods::GET, Methods::GET]),
+);
