68abd67bc06fff197c7a59ebc4302f7faceeca2e
David Grudl <david@grudl.com>
2022-05-02 12:43:03 +0200

    fixed "Logs exception if the Content-Length header prevents the Bar from being displayed" [Closes #541]

--- src/Tracy/Bar/Bar.php
+++ src/Tracy/Bar/Bar.php
@@ -84,6 +84,10 @@ class Bar
 				$redirectQueue[] = ['content' => $this->renderPartial('redirect', '-r' . count($redirectQueue)), 'time' => time()];
 			}
 		} elseif (Helpers::isHtmlMode()) {
+			if (preg_match('#^Content-Length:#im', implode("\n", headers_list()))) {
+				Debugger::log(new \LogicException('Tracy cannot display the Bar because the Content-Length header is being sent'), Debugger::EXCEPTION);
+			}
+
 			$content = $this->renderPartial('main');
 
 			foreach (array_reverse($redirectQueue) as $item) {
