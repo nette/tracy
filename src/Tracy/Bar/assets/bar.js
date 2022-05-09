a07b6ea83b1f7af9992a33ef28fad4e6000ebefb
David Grudl <david@grudl.com>
2022-05-09 13:36:25 +0200

    bar.js: options can be set after script is loaded [Closes #535]

--- src/Tracy/Bar/assets/bar.js
+++ src/Tracy/Bar/assets/bar.js
@@ -2,15 +2,23 @@
  * This file is part of the Tracy (https://tracy.nette.org)
  */
 
-let panelZIndex = 20000,
-	maxAjaxRows = 3,
-	autoRefresh = true,
-	nonce = document.currentScript.getAttribute('nonce') || document.currentScript.nonce,
+let nonce = document.currentScript.getAttribute('nonce') || document.currentScript.nonce,
 	requestId = document.currentScript.dataset.id,
 	ajaxCounter = 1,
 	baseUrl = location.href.split('#')[0];
 
 baseUrl += (baseUrl.indexOf('?') < 0 ? '?' : '&');
+
+let defaults = {
+	PanelZIndex: 20000,
+	MaxAjaxRows: 3,
+	AutoRefresh: true,
+};
+
+function getOption(key)
+{
+	return window['Tracy' + key] || defaults[key];
+}
 
 class Panel
 {
@@ -96,7 +104,7 @@ class Panel
 				Debug.panels[id].elem.classList.remove(Panel.FOCUSED);
 			}
 			elem.classList.add(Panel.FOCUSED);
-			elem.style.zIndex = panelZIndex + Panel.zIndexCounter++;
+			elem.style.zIndex = getOption('PanelZIndex') + Panel.zIndexCounter++;
 		}
 	}
 
@@ -194,7 +202,7 @@ class Panel
 		if (this.is(Panel.WINDOW)) {
 			localStorage.setItem(key, JSON.stringify({window: true}));
 		} else if (pos.width) { // is visible?
-			localStorage.setItem(key, JSON.stringify({right: pos.right, bottom: pos.bottom, width: pos.width, height: pos.height, zIndex: this.elem.style.zIndex - panelZIndex, resized: this.is(Panel.RESIZED)}));
+			localStorage.setItem(key, JSON.stringify({right: pos.right, bottom: pos.bottom, width: pos.width, height: pos.height, zIndex: this.elem.style.zIndex - getOption('PanelZIndex'), resized: this.is(Panel.RESIZED)}));
 		} else {
 			localStorage.removeItem(key);
 		}
@@ -218,7 +226,7 @@ class Panel
 				this.elem.style.height = pos.height + 'px';
 			}
 			setPosition(this.elem, pos);
-			this.elem.style.zIndex = panelZIndex + (pos.zIndex || 1);
+			this.elem.style.zIndex = getOption('PanelZIndex') + (pos.zIndex || 1);
 			Panel.zIndexCounter = Math.max(Panel.zIndexCounter, (pos.zIndex || 1)) + 1;
 		}
 	}
@@ -403,7 +411,7 @@ class Debug
 	static loadAjax(content) {
 		let rows = Debug.bar.elem.querySelectorAll('.tracy-row[data-tracy-group=ajax]');
 		rows = Array.from(rows).reverse();
-		let max = maxAjaxRows;
+		let max = getOption('MaxAjaxRows');
 		rows.forEach((row) => {
 			if (--max > 0) {
 				return;
@@ -474,7 +482,7 @@ class Debug
 		XMLHttpRequest.prototype.open = function() {
 			oldOpen.apply(this, arguments);
 
-			if (autoRefresh && new URL(arguments[1], location.origin).host === location.host) {
+			if (getOption('AutoRefresh') && new URL(arguments[1], location.origin).host === location.host) {
 				let reqId = Tracy.getAjaxHeader();
 				this.setRequestHeader('X-Tracy-Ajax', reqId);
 				this.addEventListener('load', function() {
@@ -490,7 +498,7 @@ class Debug
 			request = request instanceof Request ? request : new Request(request, options || {});
 			let reqId = request.headers.get('X-Tracy-Ajax');
 
-			if (autoRefresh && !reqId && new URL(request.url, location.origin).host === location.host) {
+			if (getOption('AutoRefresh') && !reqId && new URL(request.url, location.origin).host === location.host) {
 				reqId = Tracy.getAjaxHeader();
 				request.headers.set('X-Tracy-Ajax', reqId);
 			}
@@ -514,13 +522,6 @@ class Debug
 		Debug.scriptElem.src = url;
 		Debug.scriptElem.setAttribute('nonce', nonce);
 		(document.body || document.documentElement).appendChild(Debug.scriptElem);
-	}
-
-
-	static setOptions(options) {
-		maxAjaxRows = options.maxAjaxRows || maxAjaxRows;
-		autoRefresh = typeof options.autoRefresh !== 'undefined' ? options.autoRefresh : autoRefresh;
-		panelZIndex = options.panelZIndex || panelZIndex;
 	}
 }
 
@@ -677,9 +678,3 @@ Tracy.DebugPanel = Panel;
 Tracy.DebugBar = Bar;
 Tracy.Debug = Debug;
 Tracy.getAjaxHeader = () => requestId + '_' + ajaxCounter++;
-
-Debug.setOptions({
-	panelZIndex: Tracy.panelZIndex,
-	maxAjaxRows: window.TracyMaxAjaxRows,
-	autoRefresh: window.TracyAutoRefresh,
-});
