f919ed6d86660bec10a4246c34a77782b53020fb
David Grudl <david@grudl.com>
2022-11-18 03:28:33 +0100

    BlueScreen: fixed initialization issues, added globalInit()
    - tracy-bs-visible is set correctly when tracy is collapsed

--- src/Tracy/BlueScreen/assets/bluescreen.js
+++ src/Tracy/BlueScreen/assets/bluescreen.js
@@ -5,31 +5,14 @@
 class BlueScreen
 {
 	static init(ajax) {
+		BlueScreen.globalInit();
+
 		let blueScreen = document.getElementById('tracy-bs');
 
 		document.documentElement.classList.add('tracy-bs-visible');
 		if (navigator.platform.indexOf('Mac') > -1) {
 			blueScreen.classList.add('tracy-mac');
 		}
-
-		if (!ajax) {
-			document.body.appendChild(blueScreen);
-			let id = location.href + document.querySelector('.tracy-section--error').textContent;
-			Tracy.Toggle.persist(blueScreen, sessionStorage.getItem('tracy-toggles-bskey') === id);
-			sessionStorage.setItem('tracy-toggles-bskey', id);
-		}
-
-		if (inited) {
-			return;
-		}
-		inited = true;
-
-		// enables toggling via ESC
-		document.addEventListener('keyup', (e) => {
-			if (e.keyCode === 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) { // ESC
-				Tracy.Toggle.toggle(document.getElementById('tracy-bs-toggle'));
-			}
-		});
 
 		blueScreen.addEventListener('tracy-toggle', (e) => {
 			if (e.target.matches('#tracy-bs-toggle')) { // blue screen toggle
@@ -40,12 +23,31 @@ class BlueScreen
 			}
 		});
 
+		if (!ajax) {
+			document.body.appendChild(blueScreen);
+			let id = location.href + document.querySelector('.tracy-section--error').textContent;
+			Tracy.Toggle.persist(blueScreen, sessionStorage.getItem('tracy-toggles-bskey') === id);
+			sessionStorage.setItem('tracy-toggles-bskey', id);
+		}
+
+		(new ResizeObserver(stickyFooter)).observe(blueScreen);
+	}
+
+
+	static globalInit() {
+		// enables toggling via ESC
+		document.addEventListener('keyup', (e) => {
+			if (e.keyCode === 27 && !e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) { // ESC
+				Tracy.Toggle.toggle(document.getElementById('tracy-bs-toggle'));
+			}
+		});
+
 		Tracy.TableSort.init();
 		Tracy.Tabs.init();
 
-		// sticky footer
 		window.addEventListener('scroll', stickyFooter);
-		(new ResizeObserver(stickyFooter)).observe(blueScreen);
+
+		BlueScreen.globalInit = function() {};
 	}
 
 
@@ -67,8 +69,6 @@ function stickyFooter() {
 	footer.classList.toggle('tracy-footer--sticky', false); // to measure footer.offsetTop
 	footer.classList.toggle('tracy-footer--sticky', footer.offsetHeight + footer.offsetTop - window.innerHeight - document.documentElement.scrollTop < 0);
 }
-
-let inited;
 
 let Tracy = window.Tracy = window.Tracy || {};
 Tracy.BlueScreen = Tracy.BlueScreen || BlueScreen;
