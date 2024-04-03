823399ca88d237d5b3ce480171c2f35c6a3c44dc
David Grudl <david@grudl.com>
2024-04-03 20:38:58 +0200

    error.log changed to warning.log

--- src/Tracy/Debugger/ProductionStrategy.php
+++ src/Tracy/Debugger/ProductionStrategy.php
@@ -65,7 +65,7 @@ final class ProductionStrategy
 			$err = 'PHP ' . Helpers::errorTypeToString($severity) . ': ' . Helpers::improveError($message) . " in $file:$line";
 		}
 
-		Debugger::tryLog($err, Debugger::ERROR);
+		Debugger::tryLog($err, Debugger::WARNING);
 	}
 
 
