16d978dd5052421ce24d41aed17eaccd9d51181d
David Grudl <david@grudl.com>
2024-04-29 12:20:21 +0200

    bar: errors panel => warnings

--- src/Tracy/Debugger/DevelopmentStrategy.php
+++ src/Tracy/Debugger/DevelopmentStrategy.php
@@ -91,7 +91,7 @@ final class DevelopmentStrategy
 		}
 
 		$message = Helpers::errorTypeToString($severity) . ': ' . Helpers::improveError($message);
-		$count = &$this->bar->getPanel('Tracy:errors')->data["$file|$line|$message"];
+		$count = &$this->bar->getPanel('Tracy:warnings')->data["$file|$line|$message"];
 
 		if (!$count++ && !Helpers::isHtmlMode() && !Helpers::isAjax()) {
 			echo "\n$message in $file on line $line\n";
