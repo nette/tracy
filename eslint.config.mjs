35902aa466fe98bdaf997a4a6398dfdd40a44382
David Grudl <david@grudl.com>
2024-06-03 17:53:57 +0200

    eslint: used @stylistic rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,36 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
+	{
+		ignores: ['*/*', '!src/*'],
+	},
+
 	pluginJs.configs.recommended,
+
+	stylistic.configs.customize({
+		indent: 'tab',
+		braceStyle: '1tbs',
+		arrowParens: true,
+		semi: true,
+		jsx: false,
+	}),
+
 	{
 		languageOptions: {
 			ecmaVersion: 'latest',
 			globals: globals.browser,
 		},
+		plugins: {
+			'@stylistic': stylistic,
+		},
 		rules: {
-			indent: ['error', 'tab'],
-			quotes: ['error', 'single'],
-			semi: ['error', 'always'],
+			'@stylistic/no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
+			'@stylistic/new-parens': ['error', 'never'],
+			'@stylistic/padded-blocks': 'off',
+			'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
 			'prefer-arrow-callback': ['error'],
-			'arrow-parens': ['error'],
-			'arrow-spacing': ['error'],
 			'no-var': ['error'],
 		},
 	},
