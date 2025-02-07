0bb09ff5e29d87e7e8dceab07bb05c1ce461e4c5
David Grudl <david@grudl.com>
2025-02-07 06:59:53 +0100

    uses @nette/eslint-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,19 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
+import nette from '@nette/eslint-plugin';
+import { defineConfig } from 'eslint/config';
 
-export default [
+export default defineConfig([
 	{
-		ignores: ['*/', '!src/'],
+		ignores: [
+			'vendor', 'tests', 'x',
+		],
 	},
 
-	pluginJs.configs.recommended,
-
-	stylistic.configs.customize({
-		indent: 'tab',
-		braceStyle: '1tbs',
-		arrowParens: true,
-		semi: true,
-		jsx: false,
-	}),
-
 	{
-		languageOptions: {
-			ecmaVersion: 'latest',
-			globals: globals.browser,
-		},
-		plugins: {
-			'@stylistic': stylistic,
-		},
-		rules: {
-			'@stylistic/no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
-			'@stylistic/new-parens': ['error', 'never'],
-			'@stylistic/padded-blocks': 'off',
-			'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
-			'prefer-arrow-callback': 'error',
-			'arrow-body-style': 'error',
-			'eqeqeq': ['error', 'always', { null: 'ignore' }],
-			'no-var': 'error',
-			'prefer-const': 'off',
-		},
+		files: [
+			'*.js',
+			'src/**/*.js',
+		],
+
+		extends: [nette.configs.recommended],
 	},
-];
+]);
