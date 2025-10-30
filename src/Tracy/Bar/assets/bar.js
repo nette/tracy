27ce9b1b2dc983d3c648b281d9e8a0c9ead3b377
David Grudl <david@grudl.com>
2025-10-30 19:57:33 +0100

    cs

--- src/Tracy/Bar/assets/bar.js
+++ src/Tracy/Bar/assets/bar.js
@@ -157,9 +157,9 @@ class Panel {
 
 		let meta = this.elem.parentElement.lastElementChild;
 		doc.body.innerHTML = '<tracy-div itemscope>'
-		+ '<div class="tracy-panel tracy-mode-window" id="' + this.elem.id + '">' + this.elem.tracyContent + '</div>'
-		+ meta.outerHTML
-		+ '</tracy-div>';
+			+ '<div class="tracy-panel tracy-mode-window" id="' + this.elem.id + '">' + this.elem.tracyContent + '</div>'
+			+ meta.outerHTML
+			+ '</tracy-div>';
 		evalScripts(doc.body);
 		if (this.elem.querySelector('h1')) {
 			doc.title = this.elem.querySelector('h1').textContent;
