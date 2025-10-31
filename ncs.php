0e6c01576639d960f2d2a093d7f1860f20c42f8a
David Grudl <david@grudl.com>
2025-10-31 03:16:00 +0100

    moved Dumper tests

--- ncs.php
+++ ncs.php
@@ -10,4 +10,6 @@ declare(strict_types=1);
 return [
 	// src/Tracy/Debugger/Debugger.php
 	'ordered_class_elements' => false,
+	// tests\Dumper\dump().*.phpt
+	'PhpCsFixerCustomFixers/commented_out_function' => false,
 ];
