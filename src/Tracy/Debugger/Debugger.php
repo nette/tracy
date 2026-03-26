131cf422d927f94c0320c76034f69d24575215df
David Grudl <david@grudl.com>
2026-03-26 14:33:18 +0100

    opened 2.13-dev

--- src/Tracy/Debugger/Debugger.php
+++ src/Tracy/Debugger/Debugger.php
@@ -17,7 +17,7 @@ use const PHP_VERSION;
  */
 class Debugger
 {
-	public const Version = '2.12-dev';
+	public const Version = '2.13-dev';
 
 	/** server modes for Debugger::enable() */
 	public const
