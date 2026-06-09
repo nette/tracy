2876025a2750ba1aa1e4359c86ea0bb9ca48fbf2
David Grudl <david@grudl.com>
2026-06-09 18:09:14 +0200

    Debugger: extracted dumpOptions() and agentDumpOptions(), removes triple duplication of Dumper option assembly

--- src/Tracy/Debugger/Debugger.php
+++ src/Tracy/Debugger/Debugger.php
@@ -484,12 +484,7 @@ class Debugger
 	public static function dump(mixed $var, bool $return = false): mixed
 	{
 		if ($return) {
-			$options = [
-				Dumper::DEPTH => self::$maxDepth,
-				Dumper::TRUNCATE => self::$maxLength,
-				Dumper::ITEMS => self::$maxItems,
-				Dumper::KEYS_TO_HIDE => self::$keysToHide,
-			];
+			$options = self::dumpOptions();
 			return Helpers::isCli()
 				? Dumper::toText($var, $options)
 				: Helpers::capture(fn() => Dumper::dump($var, $options));
@@ -497,21 +492,14 @@ class Debugger
 		} elseif (!self::$productionMode) {
 			$html = Helpers::isHtmlMode();
 			echo $html ? '<tracy-div>' : '';
-			Dumper::dump($var, [
-				Dumper::DEPTH => self::$maxDepth,
-				Dumper::TRUNCATE => self::$maxLength,
-				Dumper::ITEMS => self::$maxItems,
+			Dumper::dump($var, self::dumpOptions() + [
 				Dumper::LOCATION => self::$showLocation,
 				Dumper::THEME => self::$dumpTheme,
-				Dumper::KEYS_TO_HIDE => self::$keysToHide,
 			]);
 			echo $html ? '</tracy-div>' : '';
 
 			if ($html && Helpers::isAgent()) {
-				Helpers::consoleLog(Dumper::toText($var, [
-					Dumper::DEPTH => 3,
-					Dumper::KEYS_TO_HIDE => self::$keysToHide,
-				]));
+				Helpers::consoleLog(Dumper::toText($var, self::agentDumpOptions()));
 			}
 		}
 
@@ -550,20 +538,35 @@ class Debugger
 				self::getBar()->addPanel($panel = new DefaultBarPanel('dumps'), 'Tracy:dumps');
 			}
 
-			$panel->data[] = ['title' => $title, 'dump' => Dumper::toHtml($var, $options + [
-				Dumper::DEPTH => self::$maxDepth,
-				Dumper::ITEMS => self::$maxItems,
-				Dumper::TRUNCATE => self::$maxLength,
+			$panel->data[] = ['title' => $title, 'dump' => Dumper::toHtml($var, $options + self::dumpOptions() + [
 				Dumper::LOCATION => self::$showLocation ?: Dumper::LOCATION_CLASS | Dumper::LOCATION_SOURCE,
 				Dumper::LAZY => true,
-				Dumper::KEYS_TO_HIDE => self::$keysToHide,
-			]), 'text' => Helpers::isAgent() ? Dumper::toText($var, [
-				Dumper::DEPTH => 3,
-				Dumper::KEYS_TO_HIDE => self::$keysToHide,
-			]) : null];
+			]), 'text' => Helpers::isAgent() ? Dumper::toText($var, self::agentDumpOptions()) : null];
 		}
 
 		return $var;
+	}
+
+
+	/** @return array<string, mixed> */
+	private static function dumpOptions(): array
+	{
+		return [
+			Dumper::DEPTH => self::$maxDepth,
+			Dumper::TRUNCATE => self::$maxLength,
+			Dumper::ITEMS => self::$maxItems,
+			Dumper::KEYS_TO_HIDE => self::$keysToHide,
+		];
+	}
+
+
+	/** @return array<string, mixed> */
+	private static function agentDumpOptions(): array
+	{
+		return [
+			Dumper::DEPTH => 3,
+			Dumper::KEYS_TO_HIDE => self::$keysToHide,
+		];
 	}
 
 
