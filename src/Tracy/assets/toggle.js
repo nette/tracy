608ab3625be3f936f01c2d90cc6cba964fadff92
David Grudl <david@grudl.com>
2026-06-24 01:19:22 +0200

    Toggle: keyboard activation of toggle links works

--- src/Tracy/assets/toggle.js
+++ src/Tracy/assets/toggle.js
@@ -17,7 +17,7 @@ class Toggle {
 			if (
 				!e.shiftKey && !e.ctrlKey && !e.metaKey
 				&& (el = e.target.closest('.tracy-toggle'))
-				&& Math.pow(start[0] - e.clientX, 2) + Math.pow(start[1] - e.clientY, 2) < MOVE_THRESHOLD
+				&& (!start || Math.pow(start[0] - e.clientX, 2) + Math.pow(start[1] - e.clientY, 2) < MOVE_THRESHOLD) // no start = keyboard activation
 			) {
 				Toggle.toggle(el, undefined, e);
 				e.preventDefault();
